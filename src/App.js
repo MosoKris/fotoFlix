c17ea990e1e27dd86c9beef568805527eabd8c47
MosoKris <mosomurmu@gmail.com>
2024-06-15 11:14:31 +0530

    Fixed routing issues and updated navigation links

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import './App.css';
 import React from 'react';
-import Photos from './Components/Photos';
-import Favourites from './Components/Favourites';
-import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { FaSearch } from 'react-icons/fa';
 import { useState } from 'react';
-import './App.css'; 
+import './App.css';
+import Photos from './Components/Photos';
+import Favourites from './Components/Favourites';
 
 function App() {
-
   const [searchQuery, setSearchQuery] = useState("");
   const [favouritePhotos, setFavouritePhotos] = useState([]);
 
   const handleSearch = (e) => {
     e.preventDefault();
     setSearchQuery(e.target[0].value);
-  }
+  };
 
   const handleFavourite = (photo) => {
     setFavouritePhotos((prevFavourites) => [...prevFavourites, photo]);
-  }
+  };
 
   const handleRemoveFavourite = (photoId) => {
     setFavouritePhotos((prevFavourites) =>
       prevFavourites.filter((favPhoto) => favPhoto.id !== photoId)
     );
-  }
+  };
 
   return (
-    <BrowserRouter  >
+    <Router>
       <div>
         <nav className='navbar'>
           <div className='navbar_logo'>
@@ -41,19 +39,16 @@ function App() {
             </button>
           </form>
           <div className='navbar_links'>
-            {/* <Link to="/">Home</Link> */}
+            <Link to="/">Home</Link>
             <Link to="/favourites">Favourites</Link>
           </div>
         </nav>
         <Routes>
-          <Route path='/' element={<Photos
-            searchQuery={searchQuery} onFavouriteClick={handleFavourite} />} />
-
-          <Route path='/favourites' element={<Favourites
-            favouritePhotos={favouritePhotos} handleRemoveFavourite={handleRemoveFavourite} />} />
+          <Route path='/' element={<Photos searchQuery={searchQuery} onFavouriteClick={handleFavourite} />} />
+          <Route path='/favourites' element={<Favourites favouritePhotos={favouritePhotos} handleRemoveFavourite={handleRemoveFavourite} />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
--- src/Components/Favourites.jsx
+++ src/Components/Favourites.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Photos from './Photos';
 
 const Favourites = ({ favouritePhotos, handleRemoveFavourite }) => {
@@ -7,25 +8,26 @@ const Favourites = ({ favouritePhotos, handleRemoveFavourite }) => {
       <nav className="navbar">
         <div className="navbar_logo">fotoFlix</div>
         <div className="navbar_links">
-          <a href='/'>Home</a>
+          <Link to="/">Home</Link>
         </div>
       </nav>
       <main>
         <section className="photos">
           <div className="photos-center">
-            {favouritePhotos.length > 0 ?  (
+            {favouritePhotos.length > 0 ? (
               favouritePhotos.map((image, index) => (
                 <Photos
-                    key={index}
-                    {...image}
-                    isFavourite={true}
-                    onFavouriteClick={() => handleRemoveFavourite(image.id)}>
-                      <span>Added to favourite</span>
-                    </Photos>
+                  key={index}
+                  {...image}
+                  isFavourite={true}
+                  onFavouriteClick={() => handleRemoveFavourite(image.id)}
+                >
+                  <span>Added to favourite</span>
+                </Photos>
               ))
             ) : (
               <p>No favourite photos yet.</p>
-            ) }
+            )}
           </div>
         </section>
       </main>
